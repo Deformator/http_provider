46b7747be1d5710c7a1144dab1fd12eea75db14c
Andrii Damm <andrii@unionms-iMac.local>
2018-11-01 14:14:43 -0400

    JS Docs for provider

--- src/providers/backend/backend.ts
+++ src/providers/backend/backend.ts
@@ -11,31 +11,45 @@ import { GlobalProvider } from '../global/global';
 @Injectable()
 export class BackendProvider {
 
-  protocol = 'https://';
-  domainName = 'opseuapp.org';
-  apiUrl = '/wp-json/umevents/v1/';
-  testUrl = 'https://opseuapp.org/wp-json/umevents/v1/';
-  ttl = 60 * 60 * 24 * 30;
-  token = '';
+  protocol = 'https://'; //protocol name
+  domainName = 'opseuapp.org'; //domain name
+  apiUrl = '/wp-json/umevents/v1/'; //api url
+  ttl = 60 * 60 * 24 * 30; // time after cache will expire (in milliseconds)
+  token = ''; //token
 
-  constructor(public http: HttpClient, private cache: CacheService, private storage: Storage, private global: GlobalProvider) {
+  constructor(
+    public http: HttpClient,
+    private cache: CacheService,
+    private storage: Storage,
+    private global: GlobalProvider
+  ) {
 
   }
 
+  // base shape for HTTP request 
   request(endpoint: string, requestType: string, fresh: boolean, data?: any) {
 
+    //init headers for HTTP request
     let httpOptions = {
       headers: new HttpHeaders({
         'Token': this.token
       })
     }
 
+    //observable subject which is observable and observer in the same time
     const subject = new Subject();
+
+    //url for HTTP request
     const requestedUrl = `${this.protocol}${this.domainName}${this.apiUrl}${endpoint}`;
+
+    //request varible which will be store Observable
     let request$;
+
+    //check type of HTTP request and set the proper method to request variable
     if (requestType === 'GET') {
       request$ = this.http.get(requestedUrl, httpOptions);
     } else if (requestType === 'POST') {
+      //get data object and parse it to Form Data object for sending 'Multipart form' data type
       let body: FormData = new FormData();
       Object.keys(data).forEach(key => {
         body.append(key, data[key]);
@@ -43,11 +57,12 @@ export class BackendProvider {
       request$ = this.http.post(requestedUrl, body, httpOptions);
     }
 
-
-
+    //get cached data from storage
     this.storage.get(requestedUrl)
       .then(cachedResponse => {
+        //chek if we have cached data and user don't want fresh data from a server 
         if (cachedResponse && !fresh) {
+          //return cached data
           subject.next(
             {
               dataType: 'cache',
@@ -55,57 +70,72 @@ export class BackendProvider {
             }
           );
           subject.complete();
-        } else {
+        }
+        //if we don't have cached data or user wants fresh one
+        else {
+          //chek if we have an internet connection
           if (this.global.networkAvailable) {
-            this.cache.loadFromDelayedObservable(requestedUrl, request$, 'POST', this.ttl, 'all')
+            //do request in storage then to a server
+            this.cache.loadFromDelayedObservable(requestedUrl, request$, requestType, this.ttl, 'all')
+              //returns only server response
               .pipe(
                 last()
               )
               .subscribe((res: any) => {
+                //check response status
                 if (res.status === `SUCCESS`) {
+                  //return live data from a server
                   subject.next(
                     {
                       dataType: 'live',
                       data: res
                     }
                   );
-                  // subject.complete();
-                } else {
+                  subject.complete();
+                }
+                //if server response is 200 but status wasn't SUCCESS 
+                else {
+                  //remove cached object from storage
                   this.cache.removeItem(requestedUrl);
-
-                  console.log(`Server requested with a not success status: ${res.status}`); //adjust a handler
+                  console.log(`Server requested with a not success status: ${res.status}`);
+                  //emit an error
                   subject.error(`Server requested with a not success status: ${res.status}`);
                 }
 
               },
+                //if request returns not status 200
                 (error: Response) => {
                   console.log(`Request to the server ${requestedUrl} returns an error: ${error.status}`);
-                  //  throw error;
+                  //emit an error
                   subject.error(`Request to the server ${requestedUrl} returns an error: ${error.status}`)
                 })
-          } else {
+          }
+          //network is unavailable
+          else {
+            //emit an error
             subject.error('Network is unavailable');
           }
 
         }
       })
+      //we got an error when tried to reqch a storage
       .catch(err => {
+        //emit an error
         subject.error(`Can't reach the storage. Error: ${err}`);
       })
 
     return subject;
   }
 
-  postRequest(endpoint: string, fresh = false, data) {
-    return this.request(endpoint, 'POST', fresh, data);
-  }
-
-
-
+  //GET request
   getRequest(endpoint: string, fresh = false) {
     return this.request(endpoint, 'GET', fresh);
   }
 
+  //POST request
+  postRequest(endpoint: string, fresh = false, data) {
+    return this.request(endpoint, 'POST', fresh, data);
+  }
 
 
   getHomeMenu(fresh = false): Observable<Array<any>> {
